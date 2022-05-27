5626c1df836b9150f085d44d8b44396e781bae7e
daydash <thedaydash@gmail.com>
2022-05-27 18:17:10 +0530

    mod

--- 2a.py
+++ 2a.py
@@ -34,11 +34,11 @@ for i in range(n):
     print()
 
 
-result = []
+result = [[0,0,0],[0,0,0],[0,0,0]]
 for i in range (len(A)):
     for j in range(len(B[0])):
         for k in range(len(B)):
-            result[i][j] +=A[i][k] * B[k][j]
+            result[i][j] += A[i][k] * B[k][j]
 
 print("The Result Matrix is:")
 for r in result:
